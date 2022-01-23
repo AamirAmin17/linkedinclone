a841d3645f7e12075d87e59b00ee8498c212f672
AamirAmin17 <80876153+AamirAmin17@users.noreply.github.com>
2022-01-23 14:46:51 +0500

    working on progress bar

--- components/Form.js
+++ components/Form.js
@@ -6,6 +6,7 @@ import useSWR, { mutate, trigger } from "swr";
 import { modalState } from "../atoms/modalAtom";
 import FileBase64 from "react-file-base64";
 import Resizer from "react-image-file-resizer";
+import axios from "axios";
 const Form = () => {
   const [photoUrl, setPhotoUrl] = useState("");
   const [userInfo, setUserInfo] = useState([]);
@@ -28,6 +29,7 @@ const Form = () => {
   };
 
   const handleImage = (event) => {
+    console.log("event: ", event);
     setUserInfo({ ...userInfo, file: event.target.files[0] });
   };
   //compress image file on runtime (WEBP).
@@ -35,10 +37,10 @@ const Form = () => {
     new Promise((resolve) => {
       Resizer.imageFileResizer(
         file,
-        800,
-        600,
+        1200,
+        630,
         "WEBP",
-        80,
+        85,
         0,
         (uri) => {
           resolve(uri);
@@ -66,16 +68,17 @@ const Form = () => {
     console.log("Final object", postObject);
 
     await mutate("/api/posts", [...getData, postObject], false);
-    const response = await fetch("/api/posts", {
-      method: "POST",
-      body: JSON.stringify(postObject),
-      headers: {
-        "Content-Type": "application/json",
+    const response = await axios.post("/api/posts", postObject, {
+      onUploadProgress: (ProgressEvent) => {
+        console.log(
+          "Progress uploaded",
+          Math.round((ProgressEvent.loaded / ProgressEvent.total) * 100)
+        );
       },
     });
-    const responseData = await response.json();
+    // const responseData = await response.json();
     mutate("/api/posts");
-    console.log("responseData: ", responseData);
+    console.log("responseData: ", response);
     setModalOpen(false);
   };
 
@@ -119,27 +122,25 @@ const Form = () => {
         />
       </div>
       <div className='flex items-center justify-between'>
-        {/* <FileBase64
-          multiple={false}
-          onDone={({ base64 }) => setUserInfo({ ...userInfo, image: base64 })}
-        /> */}
         <input
           type='file'
           className='bg-transparent focus:outline-none truncate w-full max-w-xs md:max-w-sm dark:placeholder-white/75'
           onChange={handleImage}
           name='upload_file'
           id='actual-btn'
+          onProgress={(e) => console.log("checking progress", e)}
+          hidden
         />
         {/* <button className="inputButton group" for="actual-btn">
           <PhotoSizeSelectActual className="text-blue-400" />
           <h4 className="opacity-80 group-hover:opacity-100">Photo</h4>
         </button> */}
-        {/* <label for="actual-btn">
-          <div className="inputButton group">
-            <PhotoSizeSelectActual className="text-blue-400" />
-            <h4 className="opacity-80 group-hover:opacity-100">Add a photo</h4>
+        <label for='actual-btn'>
+          <div className='inputButton group'>
+            <PhotoSizeSelectActual className='text-blue-400' />
+            <h4 className='opacity-80 group-hover:opacity-100'>Add a photo</h4>
           </div>
-        </label> */}
+        </label>
 
         <button
           className='font-medium bg-blue-400 hover:bg-blue-500 disabled:text-black/40 disabled:bg-white/75 disabled:cursor-not-allowed text-white rounded-full px-3.5 py-1'
--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -2,6 +2,8 @@ import { connectToDatabase } from "../../../utill/mongodb";
 import { Timestamp } from "mongodb";
 import multer from "multer";
 
+export const config = { api: { bodyParser: { sizeLimit: "4mb" } } }; // Set desired value here } } }
+
 export default async function handler(req, res) {
   const { method, body } = req;
 
